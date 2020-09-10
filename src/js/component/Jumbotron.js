d7058e70d10604046d7d7c2773df7900a7cf8ba2
naramos <naramos143@gmail.com>
2020-09-10 14:43:49 +0000

    Completed Project

--- src/js/component/Jumbotron.js
+++ src/js/component/Jumbotron.js
@@ -3,16 +3,19 @@ import React from "react";
 const Jumbotron = () => {
 	return (
 		<div className="container">
-			<div className="jumbotron mt-2">
-				<h1 className="display-4 font-weight-bold">A Warm Welcome!</h1>
+			<div className="jumbotron">
+				<h1 className="display-4 font-weight-bold text-capitalize">
+					A Warm Welcome!
+				</h1>
 				<p className="lead">
-					This is a simple hero unit, a simple jumbotron-style
-					component for calling extra attention to featured content or
-					information.
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+					do eiusmod tempor incididunt ut labore et dolore magna
+					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
 				</p>
 				<p className="lead">
 					<a
-						className="btn btn-primary btn-lg ml-5"
+						className="btn btn-primary btn-lg"
 						href="#"
 						role="button">
 						Call to action!
